9f9be7acc0a4c9812bfde76d4cec43488b2a09d4
Nate Wang <supnate@gmail.com>
2016-12-16 23:34:29 +0800

    Add route handler.

---
+++ core/route.js
@@ -0,0 +1,19 @@
+'use strict';
+
+function add(feature, componentName, urlPath) {
+
+}
+
+function remove(feature, componentName) {
+
+}
+
+function move(source, target) {
+
+}
+
+module.exports = {
+  add,
+  remove,
+  move,
+};
