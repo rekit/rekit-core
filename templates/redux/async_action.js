b1c236149cfb1d5121b248a771c335c3a93ff8e6
Nate Wang <supnate@gmail.com>
2017-02-12 12:53:24 +0800

    Don't use async/await by default when create async action.

--- templates/redux/async_action.js
+++ templates/redux/async_action.js
@@ -8,7 +8,7 @@ import {
 // Rekit uses redux-thunk for async actions:
 // https://github.com/gaearon/redux-thunk
 export function ${_.camelCase(action)}(args = {}) {
-  return (dispatch) => { // optionally you could have getState as the second parameter
+  return (dispatch) => { // optionally you can have getState as the second argument
     dispatch({
       type: ${actionTypes.begin},
     });
@@ -17,27 +17,26 @@ export function ${_.camelCase(action)}(args = {}) {
     // For example: after submit a form, you need to redirect the page to another when succeeds or show some errors message if fails.
     // It's hard to use state to manage it, but returning a promise allows you to easily achieve it.
     // e.g.: handleSubmit() { this.props.actions.submitForm(data).then(()=> {}).catch(() => {}); }
-    const promise = new Promise(async (resolve, reject) => {
-      let res;
-      try {
-        // Below is just a demo of async operation, please replace it with your own.
-        // 'args.error' here is only used for demo of error handling in tests, please also remove it and use your own.
-        res = await new Promise((resolve2, reject2) => setTimeout(() => (args.error ? reject2('error') : resolve2('success')), 20));
-      } catch (err) {
-        dispatch({
-          type: ${actionTypes.failure},
-          data: { error: err },
-        });
-        reject(err);
-        return;
-      }
-
-      // Put success action dispatch out of try/catch so that rendering errors are not converted to failure actions.
-      dispatch({
-        type: ${actionTypes.success},
-        data: res,
-      });
-      resolve(res);
+    const promise = new Promise((resolve, reject) => {
+      // doRequest is for sample purpose, replace it with your own logic.
+      const doRequest = new Promise((resolve2, reject2) => setTimeout(() => (args.error ? reject2('error') : resolve2('success')), 20));
+      doRequest.then(
+        (res) => {
+          dispatch({
+            type: ${actionTypes.success},
+            data: res,
+          });
+          resolve(res);
+        },
+        // Use rejectHandler as the second argument of then so that render errors won't be caught.
+        (err) => {
+          dispatch({
+            type: ${actionTypes.failure},
+            data: { error: err },
+          });
+          reject(err);
+        },
+      );
     });
 
     return promise;
