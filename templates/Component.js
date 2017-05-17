78522296f0173fe1e3b90941f58331b419d35aab
Nate Wang <supnate@gmail.com>
2017-05-17 20:57:01 +0800

    Update compnent template for react 15.5

--- templates/Component.js
+++ templates/Component.js
@@ -1,4 +1,4 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
 
 export default class ${_.pascalCase(component)} extends Component {
   static propTypes = {
