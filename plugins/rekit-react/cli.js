14fa80b1ff3eca9a0ce593f45bc01de1dddfee79
Nate Wang <supnate@gmail.com>
2019-05-25 15:07:48 +0800

    Add cli argument for component.

---
+++ plugins/rekit-react/cli.js
@@ -0,0 +1,21 @@
+module.exports = {
+  defineArguments({ addCmd }) {
+    // TODO: move to rekit-react plugin
+    addCmd.addArgument(['--connect', '-c'], {
+      help: 'Whether to connect to the Redux store. Only used for component.',
+      action: 'storeTrue',
+    });
+
+    // TODO: move to rekit-react plugin
+    addCmd.addArgument(['--url-path', '-u'], {
+      help: 'The url path added to react router config. Only used for page/component.',
+      dest: 'urlPath',
+    });
+
+    // TODO: move to rekit-react plugin
+    addCmd.addArgument(['--async', '-a'], {
+      help: 'Whether the action is async using redux-thunk.',
+      action: 'storeTrue',
+    });
+  },
+};
--- plugins/rekit-react/index.js
+++ plugins/rekit-react/index.js
@@ -10,6 +10,7 @@ module.exports = {
   app,
   hooks,
   prefix: require('./prefix'),
+  cli: require('./cli'),
   elements: {
     feature,
     component,
