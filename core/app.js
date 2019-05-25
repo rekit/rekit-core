f45d74f4727027defc5222afa414ac6b5d4cf173
Nate Wang <supnate@gmail.com>
2019-05-26 02:29:26 +0800

    Allow noSync when getAppTypes

--- core/app.js
+++ core/app.js
@@ -29,8 +29,8 @@ function syncAppRegistryRepo() {
   return repo.sync(appRegistry, registryDir);
 }
 
-function getAppTypes() {
-  syncAppRegistryRepo();
+function getAppTypes(args) {
+  if (args && !args.noSync) syncAppRegistryRepo();
   // const pluginsDir =
   const appTypes = ['rekit-react', 'rekit-plugin']
     .map(dir => path.join(__dirname, '../plugins', dir))
