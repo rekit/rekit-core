ec56106491bd6a3a3430abb55fff26f4bab08f59
Nate Wang <supnate@gmail.com>
2019-07-14 13:24:09 +0800

    Code clean.

--- plugins/rekit-react/cli.js
+++ plugins/rekit-react/cli.js
@@ -1,19 +1,15 @@
 module.exports = {
   defineArgs({ addCmd }) {
-
-    // TODO: move to rekit-react plugin
     addCmd.addArgument(['--connect', '-c'], {
       help: 'Whether to connect to the Redux store. Only used for component.',
       action: 'storeTrue',
     });
 
-    // TODO: move to rekit-react plugin
     addCmd.addArgument(['--url-path', '-u'], {
       help: 'The url path added to react router config. Only used for page/component.',
       dest: 'urlPath',
     });
 
-    // TODO: move to rekit-react plugin
     addCmd.addArgument(['--async', '-a'], {
       help: 'Whether the action is async using redux-thunk.',
       action: 'storeTrue',
